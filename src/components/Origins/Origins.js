b5eae0410bb30335f6cd1d8d123686891bc1b13a
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-09-23 13:10:58 -0700

    spacing

--- src/components/Origins/Origins.js
+++ src/components/Origins/Origins.js
@@ -7,6 +7,7 @@ const Origins = () => {
         <h1 className='origins_title'>Origins</h1>
         <p className='origins_tagline'>SkyFort was founded at the beginning of the pandemic to provide a secure tool 
         <br /> to keep people safe and connected while working from home.</p>
+        <br />
         <p className='origins_statement'>We were founded out of a desperate need to help individuals, businesses, and institutions 
         <br />secure their connections to the internet, while remaining a privacy-first organization. Fast 
         <br />forward to today, and we are building a network for everyone, and making network security
