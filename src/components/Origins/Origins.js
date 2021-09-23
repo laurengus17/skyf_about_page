2ac66c2898e56515bf2ad1bcb7eb98e5306b93b6
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-09-23 14:58:15 -0700

    fade

--- src/components/Origins/Origins.js
+++ src/components/Origins/Origins.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './Origins.css'
 
 const Origins = () => {
+    const [visible, setVisible] = useState(true);
+    const DOM = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        observer.observe(DOM.current);
+        return () => observer.unobserve(DOM.current);
+    }, []);
+
     return (
         <>
         <h1 className='origins_title'>Origins</h1>
-        <div className='origins_spacing'>
-        <p className='origins_tagline'>SkyFort was founded at the beginning of the pandemic to provide a secure tool 
-        <br /> to keep people safe and connected while working from home.</p>
+        <div className={`fade-in-origins ${visible ? 'visible' : ''}`}
+            ref={DOM}>
+            <p className='origins_tagline'>SkyFort was founded at the beginning of the pandemic to provide a secure tool 
+            <br /> to keep people safe and connected while working from home.</p>
         </div>
-        <p className='origins_statement'>We were founded out of a desperate need to help individuals, businesses, and institutions 
-        <br />secure their connections to the internet, while remaining a privacy-first organization. Fast 
-        <br />forward to today, and we are building a network for everyone, and making network security
-        <br /> accessible to anyone.</p>
+        <div className='origins_spacing'></div>
+        <div className={`fade-in-origins-two ${visible ? 'visible' : ''}`}
+            ref={DOM}>
+            <p className='origins_statement'>We were founded out of a desperate need to help individuals, businesses, and institutions 
+            <br />secure their connections to the internet, while remaining a privacy-first organization. Fast 
+            <br />forward to today, and we are building a network for everyone, and making network security
+            <br /> accessible to anyone.</p>
+        </div>
         </>
     )
 }
